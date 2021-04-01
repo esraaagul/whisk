3db405859d870cfa906f5bebb26d08d42c16fd1c
esraas <esrglesr@gmail.com>
2021-04-02 02:12:09 +0300

    feature: Create and define js component

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.parentElement.remove()
+    updateCartTotal()
+  }
+  
+var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+  for (var i = 0; i < removeCartItemButtons.length; i++) {
+      var button = removeCartItemButtons[i]
+      button.addEventListener('click', removeCartItem)
+}
+
